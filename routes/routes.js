b73ecb4c4ffa0eaaca103d0ebd558b9cae6e6d15
pedro-iesb <pedro.santos3@iesb.edu.br>
2024-03-22 21:32:09 -0300

    rotas

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,24 @@
 const express = require('express')
+const Produto = require('../models/Produto')
 const router = express.Router()
 
 router.get('/', function(req, res){
-
     res.json({})
-
+})
+router.get('/produtos', async function(req, res){
+    res.json( await Produto.find())
+})
+router.post('/produtos', async function(req, res){
+    res.json( await Produto.create(req.body))
+})
+router.get('/produtos/:id', async function(req, res){
+    res.json( await Produto.findById(req.params.id))
+})
+router.delete('/produtos/:id', async function(req, res){
+    res.json( await Produto.findByIdAndDelete(req.params.id))
+})
+router.put('/produtos/:id', async function(req, res){
+    res.json( await Produto.findByIdAndUpdate(req.params.id, req.body))
 })
 
 module.exports = router
